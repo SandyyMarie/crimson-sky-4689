17876f9b90d01188777dfd72c7faeb6a73b4403a
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-20 00:56:34 -0600

    test: added tests for dishes/show_spec,and dish_spec to include model testing for upcoming method

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "As a visitor When I visit a dishes show page" do
+RSpec.describe "As a visitor, when I visit a dishes show page" do
   before :each do
     @chef_1 = Chef.create!(name: "Sandy")
+
     @dish_1 = Dish.create!(name: "Cheese Pizza", description: "A classic plain cheese pizza, add toppings and bake until ready", chef_id:@chef_1.id)
+
     @ingredient_1 = @dish_1.ingredients.create!(name: "Pizza Dough")
     @ingredient_2 = @dish_1.ingredients.create!(name: "Red Sauce")
     @ingredient_3 = @dish_1.ingredients.create!(name: "Cheese")
-
-
   end
 
   it "it shows the dish's name and description" do
@@ -23,6 +23,10 @@ RSpec.describe "As a visitor When I visit a dishes show page" do
     expect(page).to have_content(@ingredient_1.name)
     expect(page).to have_content(@ingredient_2.name)
     expect(page).to have_content(@ingredient_3.name)
+  end
 
+  it 'shows the total calorie count for that dish' do
+    visit dish_path(@dish_1) 
+    expect(page).to have_content(@dish_1.calories)
   end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Dish, type: :model do
     it {should belong_to :chef}
     it {should have_many :ingredients}
   end
+
+  describe 'model testing' do
+    before :each do
+      @chef_1 = Chef.create!(name: "Sandy")
+
+      @dish_1 = Dish.create!(name: "Cheese Pizza", description: "A classic plain cheese pizza, add toppings and bake until ready", chef_id:@chef_1.id)
+
+      @ingredient_1 = @dish_1.ingredients.create!(name: "Pizza Dough", calories: 300)
+      @ingredient_2 = @dish_1.ingredients.create!(name: "Red Sauce", calories: 200)
+      @ingredient_3 = @dish_1.ingredients.create!(name: "Cheese", calories: 400)
+    end
+
+    it 'can return the total amount of calories for a given dish' do
+      expect(Dish.calories).to eq(900)
+    end
+  end
 end
