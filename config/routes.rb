f19756062598ce8d407c6bea9e08f62673471d2a
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-20 00:47:04 -0600

    feat: test1/2 for US#1 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chefs do
+    resources :dishes
+  end
+
+  resources :dishes, only: [:show] do
+    resources :ingredients
+  end
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "As a visitor When I visit a dish's show page" do
+RSpec.describe "As a visitor When I visit a dishes show page" do
   before :each do
     @chef_1 = Chef.create!(name: "Sandy")
     @dish_1 = Dish.create!(name: "Cheese Pizza", description: "A classic plain cheese pizza, add toppings and bake until ready", chef_id:@chef_1.id)
-    # @ingredient_1 = Ingredient.create!(name: "Pizza Dough", dish_id: @dish_1)
-    # @ingredient_2 = Ingredient.create!(name: "Red Sauce", dish_id: @dish_1)
-    # @ingredient_3 = Ingredient.create!(name: "Cheese", dish_id: @dish_1)
+    @ingredient_1 = @dish_1.ingredients.create!(name: "Pizza Dough")
+    @ingredient_2 = @dish_1.ingredients.create!(name: "Red Sauce")
+    @ingredient_3 = @dish_1.ingredients.create!(name: "Cheese")
+
 
   end
 
@@ -16,7 +17,7 @@ RSpec.describe "As a visitor When I visit a dish's show page" do
     expect(page).to have_content(@dish_1.description)
   end
 
-  xit "shows a list of ingredients for the dish and the chefs name" do
+  it "shows a list of ingredients for the dish and the chefs name" do
     visit dish_path(@dish_1) #check routes
     expect(page).to have_content(@dish_1.chef.name)
     expect(page).to have_content([@ingredient_1, @ingredient_2, @ingredient_3])
